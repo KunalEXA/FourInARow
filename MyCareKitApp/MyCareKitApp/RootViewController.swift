6ce87bf0fb3cb127d956239a84a35118a751d647
kunal <kunal@HealthSteps-MacBook-Pro1.local>
2017-07-11 11:32:50 -0400

    Game in perfectly running state.

---
+++ MyCareKitApp/MyCareKitApp/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  MyCareKitApp
+//
+//  Created by kunal on 7/10/17.
+//  Copyright © 2017 kunal. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
